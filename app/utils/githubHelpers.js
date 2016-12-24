ec48f82aa25e9241de7655f416d4593aee36a029
waheed <wodubayo@gmail.com>
2016-12-24 11:06:10 -0600

    refactor code to use es6/7 standards i.e 'concise object functions', 'arrow functions'

--- app/utils/githubHelpers.js
+++ app/utils/githubHelpers.js
@@ -13,20 +13,18 @@ function getRepos(username) {
 }
 
 function getTotalStars(repos) {
-    return repos.data.reduce(function (prev, current) {
-        return prev + current.stargazers_count
-    }, 0);
+    return repos.data.reduce((prev, current) => prev + current.stargazers_count, 0);
 }
 
 function getPlayersData(player) {
     return getRepos(player.login)
         .then(getTotalStars)
-        .then(function (totalStars) {
-            return {
+        .then((totalStars) => (
+            {
                 followers: player.followers,
                 totalStars: totalStars
             }
-        });
+        ))
 }
 
 function calculateScores(players) {
@@ -37,15 +35,11 @@ function calculateScores(players) {
 }
 
 export function getPlayersInfo(players) {
-    return axios.all(players.map(function (username) {
-        return getUserInfo(username);
-    })).then(function (info) {
-        return info.map(function (user) {
-            return user.data;
+    return axios.all(players.map((username) => getUserInfo(username)))
+        .then((info) => info.map((user) => user.data))
+        .catch((error) => {
+            console.warn('Error in getPlayersInfo', error)
         })
-    }).catch(function (error) {
-        console.warn('Error in getPlayersInfo', error)
-    })
 }
 
 export function battle(players) {
@@ -54,5 +48,5 @@ export function battle(players) {
 
     return axios.all([playerOneData, playerTwoData])
         .then(calculateScores)
-        .catch(function (err) { console.warn('Error in getPlayersInfo: ' + err) })
+        .catch((err) => { console.warn('Error in getPlayersInfo: ' + err) })
 }
